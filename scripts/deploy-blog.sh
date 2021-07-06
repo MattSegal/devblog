ac1673a94abcd752babd47c71374c1eb452bef8d
Matthew Segal <mattdsegal@gmail.com>
2021-07-06 13:24:41 +1000

    Add cloudflare purge

--- scripts/deploy-blog.sh
+++ scripts/deploy-blog.sh
@@ -3,15 +3,28 @@ set -e
 . ./env/bin/activate
 export PELICAN_GA="UA-103174696-6"
 export PELICAN_HOSTURL="https://mattsegal.dev"
+
+echo ">>> Purging Cloudflare cache"
+. ./scripts/secrets.sh
+PURGE_URL="https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache"
+curl -X DELETE $PURGE_URL \
+    -H "Authorization: Bearer $CLOUDFLARE_AUTH_KEY" \
+    -H "Content-Type:application/json" \
+    --data '{"purge_everything":true}'
+
 echo ">>> Building social cards"
 ./build_social_cards.py
+
 echo ">>> Cleaning"
 make clean
+
 echo ">>> Publising"
 make publish
+
 echo ">>> Deploying"
 aws s3 cp \
     --recursive \
     --acl public-read \
     ./output \
     s3://mattsegal.dev
+
